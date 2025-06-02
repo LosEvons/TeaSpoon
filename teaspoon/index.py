c74e4967d6a4a70ca761dc72f0946e7dbb290a13
LosEvons <emil.malk@gmail.com>
2025-06-02 13:08:57 +0300

    Small fix to indentation

--- teaspoon/index.py
+++ teaspoon/index.py
@@ -3,4 +3,4 @@ from teaspoon.app import create_app
 app = create_app()
 
 if __name__ == "__main__":
-  app.run(host="0.0.0.0", port=app.config["PORT"], debug=True)
+    app.run(host="0.0.0.0", port=app.config["PORT"], debug=True)
